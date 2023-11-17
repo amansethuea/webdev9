af181f53e1cf92f9bed4db02ac579d616b2fc62d
Aman Seth <amanseth2345@gmail.com>
2023-11-17 00:32:44 +0000

    Major Fix | JS Client side validation - Receptionist check-in page

--- Hotel_Management_System/JS/Receptionist_JS/checkin_validation.js
+++ Hotel_Management_System/JS/Receptionist_JS/checkin_validation.js
@@ -1,7 +1,6 @@
 var availButton = document.getElementById("checkavailbtn");
 availButton.addEventListener("click", function(event){
     var usercheckindate = new Date(document.getElementById("checkindate").value).toJSON().slice(0, 10);
-    var usercheckoutdate = new Date(document.getElementById("checkoutdate").value).toJSON().slice(0, 10);
     var today = new Date().toJSON().slice(0, 10);
     if (usercheckindate < today) {
         document.getElementById("span2").style.display = "inline";
@@ -9,6 +8,14 @@ availButton.addEventListener("click", function(event){
     else {
         document.getElementById("span2").style.display = "none";
     }
+
+    event.preventDefault();
+});
+
+var availButton = document.getElementById("checkavailbtn");
+availButton.addEventListener("click", function(event){
+    var usercheckoutdate = new Date(document.getElementById("checkoutdate").value).toJSON().slice(0, 10);
+    var today = new Date().toJSON().slice(0, 10);
     if (usercheckoutdate <= today) {
         document.getElementById("span4").style.display = "inline";
     }
