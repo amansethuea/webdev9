66877322e8ca3a45e5f4853002c478b2d48b793a
joshuanewton <josh.newton97@gmail.com>
2023-11-17 10:24:50 +0000

    Could not get gallery any better with current swiping script. Will leave here for sake of history but going to try a different swiper

---
+++ Hotel_Management_System/JS/Customer_JS/Customer_JS.js
@@ -0,0 +1,14 @@
+<!-- Initialize Swiper -->
+
+var swiper = new Swiper(".mySwiper", {
+    speed: 600,
+    parallax: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
