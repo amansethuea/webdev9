4069d91c469dfd7f1b3386cc388fb3ac889824a4
joshuanewton <josh.newton97@gmail.com>
2023-11-18 14:54:13 +0000

    The swiper is 99% done! need to add text to blurred image then add number to pagination dots. Then its done!

--- Hotel_Management_System/JS/Customer_JS/Customer_JS.js
+++ Hotel_Management_System/JS/Customer_JS/Customer_JS.js
@@ -1,14 +1,25 @@
-<!-- Initialize Swiper -->
-
-var swiper = new Swiper(".mySwiper", {
-    speed: 600,
-    parallax: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+// <!-- Initialize Swiper -->
+//
+// var swiper = new Swiper(".mySwiper", {
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         renderBullet: function (index, className) {
+//             return '<span class="' + className + '">' + (index + 1) + "</span>";
+//         },
+//     },
+// });
+document.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper(".mySwiper", {
+        speed: 600,
+        parallax: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
 });
