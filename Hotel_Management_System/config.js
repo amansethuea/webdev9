94d0600e35bce4fd1ea5ed68b92275909a979751
Aman Seth (CMP - Student) <zfb23ecu@UEA.AC.UK>
2023-11-27 15:38:27 +0000

    resolved conflict

--- Hotel_Management_System/config.js
+++ Hotel_Management_System/config.js
@@ -1,16 +1,9 @@
 const config = {
     development: {
         host: "cmpstudb-01.cmp.uea.ac.uk",
-<<<<<<< Updated upstream
         user: "xzu22dbu",
         password: "",
         database: "xzu22dbu",
-=======
-        user: "zfb23ecu",
-        password: "",
-        database: "zfb23ecu",
->>>>>>> Stashed changes
-        port: "5432"
     },
     production: {
         user: '', // env var: PGUSER  - YOUR UEA username
