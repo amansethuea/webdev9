e5249c944ea0994054a4e124c831ac8c6633d349
Aman Seth (CMP - Student) <zfb23ecu@UEA.AC.UK>
2023-11-30 22:20:08 +0000

    Fetching booking and customer details on Payment processing html - Initiated

--- Hotel_Management_System/public/JS/Customer_JS/customerDetails.js
+++ Hotel_Management_System/public/JS/Customer_JS/customerDetails.js
@@ -1,3 +1,26 @@
 const bookingDetails = JSON.parse(localStorage.getItem("roomDetails"));
 console.log(bookingDetails);
-console.log(bookingDetails.no_of_rooms);
+
+/* wrap up the data */
+function createCustomerDetailsData(guest, firstname, lastname, mobile, email) {
+    const data = {
+        guest: guest,
+        firstname: firstname,
+        lastname: lastname,
+        mobile: mobile,
+        email: email
+    };
+    return data;
+}
+
+var refButton = document.getElementById("details_submit_btn");
+refButton.addEventListener("click", function (event) {
+    const guest = document.getElementById('guest').innerText;
+    const firstname = document.getElementById('firstname').innerText;
+    const lastname = document.getElementById('lastname').innerText;
+    const mobile = document.getElementById('mobile').innerText;
+    const email = document.getElementById('email').innerText;
+    localStorage.setItem('customerDetails', JSON.stringify(createCustomerDetailsData(guest.trim(), firstname.trim(), lastname.trim(), mobile.trim(), email.trim())));
+    window.location.href="http://localhost:3000/customer/payment_process.html";
+    event.preventDefault();
+});
---
+++ Hotel_Management_System/public/JS/Customer_JS/payment_processing.js
@@ -0,0 +1,5 @@
+const bookingDetails = JSON.parse(localStorage.getItem("roomDetails"));
+console.log(bookingDetails);
+
+const customerDetails = JSON.parse(localStorage.getItem("customerDetails"));
+console.log(customerDetails);
