4bb9921345104a77ae1a9a9bef8afe63e200cc4c
Aman Seth (CMP - Student) <zfb23ecu@UEA.AC.UK>
2023-12-01 22:46:20 +0000

    room id addition in booking details local storage

--- Hotel_Management_System/public/JS/Receptionist_JS/roombooking_details.js
+++ Hotel_Management_System/public/JS/Receptionist_JS/roombooking_details.js
@@ -1,11 +1,12 @@
 /* wrap up the data */
-function createData(no_of_rooms, arrival_date, checkout_date, duration, total_cost) {
+function createData(no_of_rooms, arrival_date, checkout_date, duration, total_cost, room_data) {
     const data = {
         no_of_rooms: no_of_rooms,
         arrival_date: arrival_date,
         checkout_date: checkout_date,
         duration: duration,
-        total_cost: total_cost
+        total_cost: total_cost,
+        room_data: room_data
     };
     return data;
 }
@@ -17,7 +18,14 @@ refButton.addEventListener("click", function (event) {
     const checkout_date = document.getElementById('checkoutDate').innerText;
     const duration = document.getElementById('duration_of_stay').innerText;
     const total_cost = document.getElementById('price_of_stay').innerText;
-    localStorage.setItem('roomDetails', JSON.stringify(createData(no_of_rooms.trim(), arrival_date.trim(), checkout_date.trim(), duration.trim(), total_cost.trim())));
+    
+    const selectedRoomsList = document.querySelectorAll('.room.selected_room')
+    roomIdList = [];
+    for (let i = 0; i < selectedRoomsList.length; i ++) {
+        roomIdList.push(selectedRoomsList[i].id);
+    }
+    
+    localStorage.setItem('roomDetails', JSON.stringify(createData(no_of_rooms.trim(), arrival_date.trim(), checkout_date.trim(), duration.trim(), total_cost.trim(), roomIdList)));
     window.location.href="http://localhost:3000/receptionist/guest_details.html";
     event.preventDefault();
 });
