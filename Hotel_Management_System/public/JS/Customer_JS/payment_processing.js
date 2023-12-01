ec0892435becaaf73bd6be5a6964706586699d37
Aman Seth (CMP - Student) <zfb23ecu@UEA.AC.UK>
2023-12-01 16:56:35 +0000

    Payment related fixes - Customer Continued

--- Hotel_Management_System/public/JS/Customer_JS/payment_processing.js
+++ Hotel_Management_System/public/JS/Customer_JS/payment_processing.js
@@ -4,3 +4,8 @@ console.log(bookingDetails);
 const customerDetails = JSON.parse(localStorage.getItem("customerDetails"));
 console.log(customerDetails);
 
+var refButton = document.getElementById("complete_payment_btn");
+refButton.addEventListener("click", function (event) {
+    window.location.href="http://localhost:3000/customer/payment_summary.html";
+    event.preventDefault();
+});
