3a29bba0bc1fa3f9bfcb4fa5a9e4bd94d259c979
Aman Seth (CMP - Student) <zfb23ecu@UEA.AC.UK>
2023-12-04 18:00:15 +0000

    Customer booking by Customer & Receptionist - Backend Complete

--- Hotel_Management_System/public/JS/Receptionist_JS/payment_processing.js
+++ Hotel_Management_System/public/JS/Receptionist_JS/payment_processing.js
@@ -15,7 +15,7 @@ function createPaymentData(full_name, email, address, city, state, zip, card_no,
         zip: zip,
         card_no: card_no,
         card_expiry_month: card_expiry_month,
-        card_expiry_year:card_expiry_year
+        card_expiry_year: card_expiry_year
     };
     return data;
 }
@@ -28,15 +28,56 @@ refButton.addEventListener("click", function (event) {
     const city = document.getElementById('city').value;
     const state = document.getElementById('state').value;
     const zip = document.getElementById('zip').value;
-    const card_no = document.getElementById('cname').value;
+    const card_no = document.getElementById('ccnum').value;
     const card_expiry_month = document.getElementById('expmonth').value;
     const card_expiry_year = document.getElementById('expyear').value;
-    localStorage.setItem('setPaymentDetails', JSON.stringify(createPaymentData(full_name.trim(), email.trim(), address.trim(), 
-    city.trim(), state.trim(), zip.trim(), card_no.trim(), card_expiry_month.trim(), card_expiry_year.trim())));
+    localStorage.setItem('setPaymentDetails', JSON.stringify(createPaymentData(full_name.trim(), email.trim(), address.trim(),
+        city.trim(), state.trim(), zip.trim(), card_no.trim(), card_expiry_month.trim(), card_expiry_year.trim())));
 
     const getPaymentDetails = JSON.parse(localStorage.getItem("setPaymentDetails"));
-    console.log(getPaymentDetails);
 
-    //window.location.href="http://localhost:3000/customer/payment_summary.html";
+
+    /* wrap up the data */
+    function createData(full_name, email, address, city, state, zip, card_no, card_expiry_month, card_expiry_year, room_data, checkin, checkout, total_cost) {
+        const data = {
+            full_name: full_name,
+            email: email,
+            address: address,
+            city: city,
+            state: state,
+            zip: zip,
+            card_no: card_no,
+            card_expiry_month: card_expiry_month,
+            card_expiry_year: card_expiry_year,
+            room_data: room_data,
+            checkin: checkin,
+            checkout: checkout,
+            total_cost: total_cost
+        };
+        return JSON.stringify(data);
+    }
+
+    /* create send meta data */
+    function createOptions(data) {
+        const fetchOptions = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: data
+        }
+
+        return fetchOptions;
+    }
+
+    const data = createData(getPaymentDetails.full_name, getPaymentDetails.email, getPaymentDetails.address, getPaymentDetails.city, getPaymentDetails.state,
+        getPaymentDetails.zip, getPaymentDetails.card_no, getPaymentDetails.card_expiry_month, getPaymentDetails.card_expiry_year, bookingDetails.room_data,
+        bookingDetails.arrival_date, bookingDetails.checkout_date, bookingDetails.total_cost);
+    const fetchOptions = createOptions(data);
+
+    fetch('http://localhost:3000/api/customer/newbooking', fetchOptions)
+        .then(window.location.href="http://localhost:3000/customer/payment_summary.html");
+
     event.preventDefault();
 });
