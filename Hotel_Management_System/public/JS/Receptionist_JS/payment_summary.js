35842772e141f5b185aaf35753ce1b1117df2646
Aman Seth (CMP - Student) <zfb23ecu@UEA.AC.UK>
2023-12-04 22:19:05 +0000

    Rendering response on UI - Receptionist :: Complete

---
+++ Hotel_Management_System/public/JS/Receptionist_JS/payment_summary.js
@@ -0,0 +1,33 @@
+const getPaymentDetails = JSON.parse(localStorage.getItem("setPaymentDetails"));
+console.log(getPaymentDetails);
+
+const bookingDetails = JSON.parse(localStorage.getItem("roomDetails"));
+console.log(bookingDetails);
+
+const customerDetails = JSON.parse(localStorage.getItem("customerDetails"));
+console.log(customerDetails);
+
+function onTextReadyForReceptionistRefNo(text) {
+    // Log the server response
+    console.log('Server:', text);
+    const data = JSON.parse(text);
+    getRefNo = data.results[0]['max'];
+    document.getElementById("b_ref").innerText = getRefNo;
+    document.getElementById("amount").innerText = bookingDetails.total_cost;
+    document.getElementById("checkin").innerText = bookingDetails.arrival_date;
+    document.getElementById("checkout").innerText = bookingDetails.checkout_date;
+    document.getElementById("guest").innerText = customerDetails.guest;
+    document.getElementById("receiveremail").innerText = customerDetails.email;
+    document.getElementById("personName").innerText = customerDetails.firstname;
+}
+
+function onResponseForReceptionistRefNo(response) {
+    // Extract the text from the response object and return it
+    return response.text();
+}
+
+window.onload = function () {
+    fetch('http://localhost:3000/customer/getref')
+        .then(onResponseForReceptionistRefNo)
+        .then(onTextReadyForReceptionistRefNo);
+}
