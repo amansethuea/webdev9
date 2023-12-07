ddd3c8cc475fadeae011611f4643f88a270f05da
Aman Seth (CMP - Student) <zfb23ecu@UEA.AC.UK>
2023-12-07 12:06:03 +0000

    Customer | Local Storage Glitches fix | Client Side validations Merged into a single JS | Implemented client side validations using JS for dates

--- Hotel_Management_System/public/JS/Customer_JS/customerDetails.js
+++ Hotel_Management_System/public/JS/Customer_JS/customerDetails.js
@@ -2,7 +2,7 @@ const bookingDetails = JSON.parse(localStorage.getItem("roomDetails"));
 console.log(bookingDetails);
 
 /* wrap up the data */
-function createCustomerDetailsData(guest, firstname, lastname, mobile, email) {
+function createGuestDetailsData(guest, firstname, lastname, mobile, email) {
     const data = {
         guest: guest,
         firstname: firstname,
@@ -20,7 +20,68 @@ refButton.addEventListener("click", function (event) {
     const lastname = document.getElementById('lastname').value;
     const mobile = document.getElementById('mobile').value;
     const email = document.getElementById('email').value;
-    localStorage.setItem('customerDetails', JSON.stringify(createCustomerDetailsData(guest.trim(), firstname.trim(), lastname.trim(), mobile.trim(), email.trim())));
-    window.location.href="http://localhost:3000/customer/payment_process.html";
+
+    if (guest == "" || guest == 0) {
+        document.getElementById("span1").style.display = "inline";
+    }
+    else {
+        document.getElementById("span1").style.display = "none";
+    }
+    if (firstname == "") {
+        document.getElementById("span4").style.display = "inline";
+    }
+    else {
+        document.getElementById("span4").style.display = "none";
+    }
+    if (/^[a-zA-Z ]*$/.test(firstname) == true) {
+        document.getElementById("span5").style.display = "none";
+    }
+    else {
+        document.getElementById("span5").style.display = "inline";
+    }
+    if (lastname == "") {
+        document.getElementById("span6").style.display = "inline";
+    }
+    else {
+        document.getElementById("span6").style.display = "none";
+    }
+    if (/^[a-zA-Z ]*$/.test(lastname) == true) {
+        document.getElementById("span7").style.display = "none";
+    }
+    else {
+        document.getElementById("span7").style.display = "inline";
+    }
+    if (/^\(?([0-9]{4})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test(mobile) == true) {
+        document.getElementById("span8").style.display = "none";
+    }
+    else {
+        document.getElementById("span8").style.display = "inline";
+    }
+
+    // email check validation including blank field check
+    if (/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(email) == true) {
+        document.getElementById("span9").style.display = "none";
+        if (localStorage.getItem('customerDetails') != null) {
+            console.log("Previous data found in local storage customerDetails. Clearing it first before proceeding.")
+            localStorage.removeItem('customerDetails');
+            localStorage.setItem('customerDetails', JSON.stringify(createGuestDetailsData(guest.trim(), firstname.trim(), lastname.trim(), mobile.trim(), email.trim())));
+            const customerDetails = localStorage.getItem("customerDetails");
+            console.log("Created new local storage data for customerDetails");
+            console.log(customerDetails);
+            window.location.href = "http://localhost:3000/customer/payment_process.html";
+        }
+        else {
+            localStorage.setItem('customerDetails', JSON.stringify(createGuestDetailsData(guest.trim(), firstname.trim(), lastname.trim(), mobile.trim(), email.trim())));
+            console.log("No previous data in local storage found for customerDetails. Creating fresh..");
+            const customerDetails = localStorage.getItem("customerDetails");
+            console.log(customerDetails);
+            window.location.href = "http://localhost:3000/customer/payment_process.html";
+        }
+    }
+    else {
+        document.getElementById("span9").style.display = "inline";
+    }
+
     event.preventDefault();
+
 });
--- Hotel_Management_System/public/JS/Customer_JS/roombooking_details.js
+++ Hotel_Management_System/public/JS/Customer_JS/roombooking_details.js
@@ -13,19 +13,79 @@ function createData(no_of_rooms, arrival_date, checkout_date, duration, total_co
 
 var refButton = document.getElementById("reserve_room_btn");
 refButton.addEventListener("click", function (event) {
-    const no_of_rooms = document.getElementById('roomsCount').innerText;
-    const arrival_date = document.getElementById('checkinDate').innerText;
-    const checkout_date = document.getElementById('checkoutDate').innerText;
-    const duration = document.getElementById('duration_of_stay').innerText;
-    const total_cost = document.getElementById('price_of_stay').innerText;
 
-    const selectedRoomsList = document.querySelectorAll('.room.selected_room')
-    roomIdList = [];
-    for (let i = 0; i < selectedRoomsList.length; i ++) {
-        roomIdList.push(selectedRoomsList[i].id);
+    const checkin = new Date(document.getElementById("booking-in-date").value).toJSON();
+    const checkout = new Date(document.getElementById("booking-out-date").value).toJSON();
+    const today = new Date().toJSON().slice(0, 10);
+
+    if (checkin < today) {
+        document.getElementById("span1").style.display = "inline";
     }
-    
-    localStorage.setItem('roomDetails', JSON.stringify(createData(no_of_rooms.trim(), arrival_date.trim(), checkout_date.trim(), duration.trim(), total_cost.trim(), roomIdList)));
-    window.location.href="http://localhost:3000/customer/customer_details.html";
+    else {
+        document.getElementById("span1").style.display = "none";
+    }
+    if (!checkin) {
+        document.getElementById("span2").style.display = "inline";
+    }
+    else {
+        document.getElementById("span2").style.display = "none";
+    }
+
+    if (checkout <= today) {
+        document.getElementById("span3").style.display = "inline";
+    }
+    else {
+        document.getElementById("span3").style.display = "none";
+    }
+    if (!checkout) {
+        document.getElementById("span4").style.display = "inline";
+    }
+    else if (checkout <= checkin) {
+        document.getElementById("span5").style.display = "inline";
+        document.getElementById("span4").style.display = "none";
+    }
+    else {
+        document.getElementById("span5").style.display = "none";
+        if (localStorage.getItem('roomDetails') != null) {
+            console.log("Previous data found in local storage roomDetails. Clearing it first before proceeding.")
+            localStorage.removeItem('roomDetails');
+            const no_of_rooms = document.getElementById('roomsCount').innerText;
+            const arrival_date = document.getElementById('checkinDate').innerText;
+            const checkout_date = document.getElementById('checkoutDate').innerText;
+            const duration = document.getElementById('duration_of_stay').innerText;
+            const total_cost = document.getElementById('price_of_stay').innerText;
+
+            const selectedRoomsList = document.querySelectorAll('.room.selected_room')
+            roomIdList = [];
+            for (let i = 0; i < selectedRoomsList.length; i++) {
+                roomIdList.push(selectedRoomsList[i].id);
+            }
+            localStorage.setItem('roomDetails', JSON.stringify(createData(no_of_rooms.trim(), arrival_date.trim(), checkout_date.trim(), duration.trim(), total_cost.trim(), roomIdList)));
+            const bookingDetails = localStorage.getItem("roomDetails");
+            console.log("Created new local storage data for roomDetails");
+            console.log(bookingDetails);
+            window.location.href = "http://localhost:3000/customer/customer_details.html";
+
+        }
+        else {
+            const no_of_rooms = document.getElementById('roomsCount').innerText;
+            const arrival_date = document.getElementById('checkinDate').innerText;
+            const checkout_date = document.getElementById('checkoutDate').innerText;
+            const duration = document.getElementById('duration_of_stay').innerText;
+            const total_cost = document.getElementById('price_of_stay').innerText;
+
+            const selectedRoomsList = document.querySelectorAll('.room.selected_room')
+            roomIdList = [];
+            for (let i = 0; i < selectedRoomsList.length; i++) {
+                roomIdList.push(selectedRoomsList[i].id);
+            }
+            localStorage.setItem('roomDetails', JSON.stringify(createData(no_of_rooms.trim(), arrival_date.trim(), checkout_date.trim(), duration.trim(), total_cost.trim(), roomIdList)));
+            console.log("No previous data in local storage found for roomDetails. Creating fresh..");
+            const bookingDetails = localStorage.getItem("roomDetails");
+            console.log(bookingDetails);
+
+        }
+    }
+
     event.preventDefault();
 });
