f822ea0943922a1d484a53d2d3f25941aaa2274b
joshuanewton <josh.newton97@gmail.com>
2023-12-07 14:13:36 +0000

    Fixed the city of norwich styling + removed the styling and js script from HTML.
    Also added the code to receptionist_style.css that removes the list of rooms on initial page load.

---
+++ Hotel_Management_System/public/JS/Customer_JS/city_of_norwich_swiper.js
@@ -0,0 +1,24 @@
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 30000,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    on: {
+        autoplayTimeLeft(s, time, progress) {
+            progressCircle.style.setProperty("--progress", 1 - progress);
+            progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        }
+    }
+});
